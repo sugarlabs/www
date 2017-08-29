725368d3bc881bf70b9bf84cf4d5a878426f1035
P Seetaramaraju <psramaraju@gmail.com>
2017-08-29 12:53:43 +0530

    GSOC 2017 project final code pull request
    ### Introduction
    This is the pull request with the final code changes I made as part of
    the GSOC 2017 project **Giving Sugar Labs Website a New Look**. The
    changes made during the **GSOC** period can be viewed at
    [https://github.com/sugarlabs/www-sugarlabs/issues/63](https://github.co
    m/sugarlabs/www-sugarlabs/issues/63).
    ### Note
    The work during **GSOC** was done in the repository
    [https://github.com/geekrypter/sugarLabsWebsiteRedesign](https://github.
    com/geekrypter/sugarLabsWebsiteRedesign). A live preview of the website
    is hosted using **Github pages** and can be viewed at
    [https://geekrypter.github.io/sugarLabsWebsiteRedesign/](https://geekryp
    ter.github.io/sugarLabsWebsiteRedesign/). However the repository
    [https://github.com/geekrypter/sugarLabsWebsiteRedesign](https://github.
    com/geekrypter/sugarLabsWebsiteRedesign) is not a forked version of the
    repository
    [https://github.com/sugarlabs/www-sugarlabs](https://github.com/sugarlab
    s/www-sugarlabs). So for the pull request, I created a forked
    repository at
    [https://github.com/geekrypter/www-sugarlabs](https://github.com/geekryp
    ter/www-sugarlabs) and under the branch
    **GSOC-2017-project-final-code**, I created this pull request.
    ### Commits
    The link where you can see my commits is -
    [https://github.com/geekrypter/sugarLabsWebsiteRedesign/commits/master](
    https://github.com/geekrypter/sugarLabsWebsiteRedesign/commits/master).
    You can check the timestamps to see the commits related to any
    particular day.
    ### Documentation
    For documentation, please check the **documentation.md** file.
    ### Future developement and improvements
    1. The website has been targetted to work on as many browsers as
    possible. However, it can be made more compatible for OLPC as there are
    certain issues:
    a. Please follow this [mail
    thread](http://lists.sugarlabs.org/archive/sugar-devel/2017-August/05453
    0.html) for the plugins issue and CPU usage issue.
    b. This is the way the website is rendered on OLPC XO 1.5 running
    latest Sugar as reported by Samson Goddy. -
    [https://drive.google.com/file/d/0B0pU4Ahiyt7UeDdFYXdfczd4bE0/view?usp=s
    haring](https://drive.google.com/file/d/0B0pU4Ahiyt7UeDdFYXdfczd4bE0/vie
    w?usp=sharing)
    whereas, this is the way the website is loaded on Fedora SOAS (This how
    it should be loaded in "Browse" activity).
    -[https://drive.google.com/open?id=0B5pN2u8B5LwcX1IteGl4czhoQjA](https:/
    /drive.google.com/open?id=0B5pN2u8B5LwcX1IteGl4czhoQjA)
    2. The website is crashing on Chromium 60.0.3112.89 (dev build) while
    using Raspbian Stretch. This issue is quite odd and doesn't show proper
    trace. It has to be further inspected and solved.

--- README.md
+++ README.md
@@ -1,15 +1,53 @@
-## SugarLabs.org Website Sources
+# Redesigned version of "www.sugarlabs.org"
 
-This website is created using Jekyll, Bootstrap, some plugins and pure jQuery.
+## Introduction
+This repository contains the code for the redesigned version of "www.sugarlabs.org". This repository code base was used for the development of the GSOC 2017 project **Giving Sugar Labs Website a New Look** under the organization **Sugar Labs**. The changes made during the **GSOC** period can be viewed at [https://github.com/sugarlabs/www-sugarlabs/issues/63](https://github.com/sugarlabs/www-sugarlabs/issues/63). The repository of the original website is located at [https://github.com/sugarlabs/www-sugarlabs](https://github.com/sugarlabs/www-sugarlabs). The work during **GSOC** was done in the repository [https://github.com/geekrypter/sugarLabsWebsiteRedesign](https://github.com/geekrypter/sugarLabsWebsiteRedesign). A live preview of the website is hosted using **Github pages** and can be viewed at [https://geekrypter.github.io/sugarLabsWebsiteRedesign/](https://geekrypter.github.io/sugarLabsWebsiteRedesign/). The code uses the same Jekyll environment as that of the original website's code. Do check out the code! 
 
-Suggestions are welcome!
+### Major Services
+We have integrated 4 major features with the help of third party services:
+1. Website users tracking - We have used the [Google Analytics](https://analytics.google.com/) service to track user on the website and get a more detailed analysis on the users usage.
+2. IRC chat - We have used the [Kiwi IRC](https://kiwiirc.com) service to enable users to chat on our IRC channels in the website itself.
+3. Newsletter subscription through email - We have used the [Mailchimp](https://mailchimp.com) service to let users to subscribe to our newsletters through their emails.
+4. Customized search functionality - We have used [Google custom search engine](https://www.google.co.in/cse) to provide a customized search for users.
 
+## Documentation
+For documentation, please check the **documentation.md** file.
+
+## License Information
+The license information of all the dependencies/libraries used in the code can be found in the **LicenseInformation.txt** file in the root folder.
+
+### Code and Contribution
+The code has been restructured, unnecessary code has been removed, comments were added wherever required, proper indentation was provided, and simple names were used which makes it a contributor-friendly codebase. So, hope on and contribute now!
+
+## Feedback/Suggestions and Issues
+Feedback/Suggestions are always welcome and do mention any issues found. They can be provided through:
+1)Issues section of this repository
+2)[https://github.com/sugarlabs/www-sugarlabs/issues/63](https://github.com/sugarlabs/www-sugarlabs/issues/63)
+
+## How to run the code
+### Getting the code
+Clone the repository
+### Installation
 Install Ruby and the Gem package manager (pre-installed in Mac OS X) and install `jekyll` and its related packages available in the Github hosted version by running, from this directory:
 
     bundle install;
-
-Jekyll can run a local web server that rebuilds each time you save a page while editing:
+### Running
+Jekyll can run a local web server that rebuilds each time you save a page while editing (Execute the below command from root directory):
 
     jekyll serve --incremental;
 
 Then open [http://localhost:4000](http://localhost:4000)
+### Note
+If some changes are not visible (after saving a page while editing), please restart the Jekyll local web server and reload [http://localhost:4000](http://localhost:4000).
+
+## Important points to be noted
+
+1. Whenever the code is changed, please ensure that the relevant code changes are reflected in **main.js** file (For example, if a slideshow is disabled, make sure the id of the slideshow element/section is also disabled in the **main.js** file if it exists).
+2. Whenever a new commit is made and the changes are made to any one of airspace.css, main.js, or plugins.js files, try to use a minification service and add the minified version of new code in respective **minified** folder. (css and js folders have a **minified** folder)
+3. Few dependencies/libraries do not work well with other dependencies/libraries, so make sure to test before updating the dependencies/libraries.
+4. Whenever the website hosting is shifted to another url/domain, please ensure to change the internal url references and also the **sitemap.txt** file.
+5. Do check **Answers to few questions** section to get answers to few of the questions you might have or for steps to be followed.
+6. Be careful when changing the cache control code present in the **.htaccess** file.
+7. Whenever the dependency/library versions are changed, please update the local files of those dependencies as these local files are referenced when the cdn does not work.
+8. The current website uses customized versions of few social media icons (service provided by [Social Share Kit](http://socialsharekit.com)). So we use the local files of the service and do our customizations in the css file of the service (**/assets/dist/css/social-share-kit.css** file in our case). Please be careful when updating the files of this service. (Also refer to **How to change social media icon colors?** subsection in **Answers to few questions** section)
+
--- js/main.js
+++ js/main.js
@@ -1,50 +1,138 @@
-// @license magnet:?xt=urn:btih:0b31508aeb0634b347b8270c7bee4d411b5d4109&dn=agpl-3.0.txt AGPL-v3-or-later
+$(document).ready(function(){
+    // Colour changing affect of logo
+    var COLORS = [['006E00', '78E600'], ['68008C', '78E600'],
+                  ['006E00', '00BEFF'], ['033CD2', '00BEFF'],
+                  ['6E008C', '00BEFF'], ['A00000', 'FF7800'],
+                  ['033CD2', 'FF64D2'], ['6E008C', 'FF64D2'],
+                  ['6E008C', 'FF7800'], ['033CD2', 'FF7800'],
+                  ['006E00', 'FFD732'], ['6E008C', 'FFD732']];
+    var color = Math.floor((Math.random() * 12));
+    var logoID = color + 1;
+    if (color < 10) {
+        logoID = "0" + logoID;
+    }
+    document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
+    var h = document.querySelector('.logo1').innerHTML;
+    h = h.replace(/033cd2/g, COLORS[color][0]);
+    h = h.replace(/78e600/g, COLORS[color][1]);
+    document.querySelector('.logo1').innerHTML = h;
+    if(document.getElementById("secondHeader").style.display!="none"){
+        var h = document.querySelector('.logo2').innerHTML;
+        h = h.replace(/033cd2/g, COLORS[color][0]);
+        h = h.replace(/78e600/g, COLORS[color][1]);
+        document.querySelector('.logo2').innerHTML = h;}
+    // Footer links collapse code
+    $( ".widget h4" ).click(
+        function() {
+            $(this).parent().toggleClass('active');
+    }); 
+    // Disabling code
+    // Opera 8.0+
+    var isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    // Firefox 1.0+
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    // Safari 3.0+ "[object HTMLElementConstructor]" 
+    var isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+    // Internet Explorer 6-11
+    var isIE = /*@cc_on!@*/false || !!document.documentMode;
+    // Edge 20+
+    var isEdge = !isIE && !!window.StyleMedia;
+    // Chrome 1+
+    var isChrome = !!window.chrome && !!window.chrome.webstore;
+    var isOtherdevice= navigator.userAgent.match(/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile/i);
+    var isSmallWidth=0;
+    if(window.innerWidth<768)
+    {
+        isSmallWidth=1;
+    }
+    if(isOpera || isFirefox || isSafari || isIE || isEdge || isChrome || isOtherdevice || isSmallWidth){
+        $('#loaderDiv').css('display', 'block');
+        $('#animationPart1').addClass('start');
+        $('#animationPart2').addClass('start');
+        $('#animationPart3').addClass('start');
+        document.getElementById("myiframe1").src = 'https://kiwiirc.com/client/irc.kiwiirc.com/?&theme=cli#sugar';
+        document.getElementById("myiframe2").src = 'https://kiwiirc.com/client/irc.kiwiirc.com/?&theme=cli#sugar';
+        if(isIE){
+            $('#navh2').css('display', 'block');
+            $('#navh3').css('display', 'block');
+        }
+        else{
+            $('#navh').css('display', 'block');
+            $('#navh1').css('display', 'block');
+        }
+    }
+    if(!isOpera && !isFirefox &&!isSafari && !isIE && !isEdge &&!isChrome &&!isOtherdevice &&!isSmallWidth){
+        $('#ftctn').css('display', 'none');
+        $('#myBtn2').css('display', 'none');
+        $('#navh2').css('display', 'block');
+        $('#navh').css('display', 'none');
+        $('#navh3').css('display', 'block');
+        $('#navh1').css('display', 'none');
+    }
+    // Chat window related code
+    var $this = $(".panel-heading span.icon_minim");
+    $this.addClass('panel-collapsed');
+    $this.removeClass('ion-minus-round').addClass('ion-plus-round');
+    $('[data-toggle="tooltip"]').tooltip(); 
+    $('[data-toggle="popover"]').popover();
+    $('.popover-dismiss').popover({
+      trigger: 'focus'
+    }) 
+    // Slideshows code
+    $("#testimonial-slider1").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#testimonial-slider2").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    }); 
+    $("#testimonial-slider3").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#testimonial-slider4").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#testimonial-slider5").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#testimonial-slider6").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#testimonial-slider7").owlCarousel({
+        paginationSpeed : 500,      
+        singleItem:true,
+        autoPlay: 3000,
+    });
+    $("#social-media-logos").owlCarousel({
+    	autoPlay: 3000,
+    	items : 5,
+    	itemsDesktop : [1199,5],
+    	itemsDesktopSmall : [979,5],
+    });	
+    var fullHeight = function() {
+    	$('.js-fullheight').css('height', $(window).height());
+    	$(window).resize(function(){
+    		$('.js-fullheight').css('height', $(window).height());
+    	});
+    };
+    // Code to detect if chat window has been closed and making sure it doent open up in other pages
+    if(window.name=="websiteChatClosed"){
+        document.getElementById("myiframe1").src = '';
+        document.getElementById("myiframe2").src = '';
+        $('#ftctn').css('display', 'none');
+        $('#myBtn2').css('display', 'none');
+    }
+});
 
-var COLORS = [['006E00', '78E600'], ['68008C', '78E600'],
-              ['006E00', '00BEFF'], ['033CD2', '00BEFF'],
-              ['6E008C', '00BEFF'], ['A00000', 'FF7800'],
-              ['033CD2', 'FF64D2'], ['6E008C', 'FF64D2'],
-              ['6E008C', 'FF7800'], ['033CD2', 'FF7800'],
-              ['006E00', 'FFD732'], ['6E008C', 'FFD732']];
 
-var color = Math.floor((Math.random() * 12));
-var logoID = color + 1;
-if (color < 10) {
-    logoID = "0" + logoID;
-}
-
-document.querySelector('#defaultIcon').href = 
-    'assets/favicon_' + logoID + '.png';
-
-var h = document.querySelector('.logo').innerHTML;
-h = h.replace(/033cd2/g, COLORS[color][0]);
-h = h.replace(/78e600/g, COLORS[color][1]);
-document.querySelector('.logo').innerHTML = h;
-
-// Only for the home page, but whatever
-// https://developer.mozilla.org/en-US/docs/Browser_detection_using_the_user_agent#OS
-
-function hide(e) {
-    e.style.display = 'none';
-}
-
-var ua = navigator.userAgent.toLowerCase();
-var get = {
-    soas: document.querySelector('#get-soas'),
-    desktop: document.querySelector('#get-desktop'),
-    linux: document.querySelector('#get-sugar-linux'),
-    sugarizer: document.querySelector('#get-sugarizer')
-};
-var l = document.querySelector('.creator-link');
-
-if (ua.indexOf('mobile') !== -1 || ua.indexOf('tablet') !== -1) {
-    hide(get.soas);  hide(get.linux);
-} else if (ua.indexOf('linux') !== -1) {
-    hide(get.desktop);  hide(get.sugarizer);
-    //l.href = '/creator/linux';
-} else if (ua.indexOf('windows') !== -1) {
-    hide(get.desktop);  hide(get.linux);  hide(get.sugarizer);
-    //l.href = '/creator/windows';
-} else {}
-
-// @license-end
