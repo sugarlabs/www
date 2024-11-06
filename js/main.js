4c3d596a5d9f237ea8445b263895b615fedc125c
therealharshit <therealharshit014@gmail.com>
2024-11-06 23:43:08 +0530

    added js code in custom.js file and linked it

--- js/main.js
+++ js/main.js
@@ -172,19 +172,3 @@ document.addEventListener('DOMContentLoaded', () => {
         observer.observe(counter); // Observe each counter
     });
 });
-
-//show answer and change indicator for /FAQ page
-function toggleAnswer(answerId, element) {
-    const answer = document.getElementById(answerId);
-    const indicator = element.querySelector('.indicator'); 
-
-    if (answer) {
-        answer.classList.toggle('d-none');
-        
-        if (answer.classList.contains('d-none')) {
-            indicator.innerHTML = '+';
-        } else {
-            indicator.innerHTML = '-';
-        }
-    }
-}
