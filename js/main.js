sugarlabs/www

commit 58177ead48db03d571d671691715414545fcfa27

Fix favicon "not found" error on non-homepage pages
- Updated `_layouts/base.html` to use a consistent favicon path.
- Modified `js/main.js` to handle dynamic favicon changes with error handling.
- Added fallback to the default favicon if a specific favicon is not found.

_layouts/base.html
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
- 		<link id="defaultIcon1" rel="shortcut icon" href="{{ site.baseurl }}assets/favicon_06.png" />
+        <link id="defaultIcon1" rel="shortcut icon" href="/assets/favicon.png" />
         <title>{{ page.title }}</title>
         <link href="{{ site.baseurl }}/css/googleFonts.css" rel="stylesheet" type="text/css" >
         <meta name="robots" content="index,follow,NOODP" />

js/main.js
@@ -16,7 +16,24 @@ $(document).ready(function () {
     if (logoID < 10) {
         logoID = "0" + logoID;
     }
-    document.querySelector('#defaultIcon1').href = 'https://www.sugarlabs.org/assets/favicon_' + logoID + '.png';
+
+    var defaultIcon = document.querySelector('#defaultIcon1');
+    if (defaultIcon) {
+        // Use absolute path for favicon
+        var faviconPath = '/assets/favicon_' + logoID + '.png';
+        
+        // Test if favicon exists before setting
+        var img = new Image();
+        img.onload = function() {
+            defaultIcon.href = faviconPath;
+        };
+        img.onerror = function() {
+            // Fallback to default favicon
+            defaultIcon.href = '/assets/favicon.png';
+        };
+        img.src = faviconPath;
+    }
+
     var h = document.querySelector('.logo1').innerHTML;
     h = h.replace(/033cd2/g, selectedColors[0]);
     h = h.replace(/78e600/g, selectedColors[1]);

commit 15f9065f1aa705e3863ec1a60fa47d40437c5d96

Fix favicon path issue on non-homepage pages in Firefox
- Fixed "favicon not found" error on non-homepage pages, particularly in Firefox.
- Problem traced to incorrect favicon path introduced in commit 58177ea.
- Updated path from absolute to relative to resolve the issue.
- Ensures favicons load correctly across all pages.

Fixes #507

_layouts/base.html
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link id="defaultIcon1" rel="shortcut icon" href="/assets/favicon.png" />
+        <link id="defaultIcon1" rel="shortcut icon" href="{{ site.baseurl }}assets/favicon_06.png" />
         <title>{{ page.title }}</title>
         <link href="{{ site.baseurl }}/css/googleFonts.css" rel="stylesheet" type="text/css" >
         <meta name="robots" content="index,follow,NOODP" />

js/main.js
@@ -19,19 +19,11 @@ $(document).ready(function () {
 
     var defaultIcon = document.querySelector('#defaultIcon1');
     if (defaultIcon) {
-        // Use absolute path for favicon
-        var faviconPath = '/assets/favicon_' + logoID + '.png';
-        
-        // Test if favicon exists before setting
-        var img = new Image();
-        img.onload = function() {
-            defaultIcon.href = faviconPath;
-        };
-        img.onerror = function() {
-            // Fallback to default favicon
-            defaultIcon.href = '/assets/favicon.png';
-        };
-        img.src = faviconPath;
+        var logoID = colorIndex + 1;
+        if (logoID < 10) {
+            logoID = "0" + logoID;
+        }
+        defaultIcon.href = 'assets/favicon_' + logoID + '.png';
     }
 
     var h = document.querySelector('.logo1').innerHTML;