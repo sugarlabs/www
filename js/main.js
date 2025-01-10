sugarlabs/www

commit 15f9065f1aa705e3863ec1a60fa47d40437c5d96

Fix favicon path issue on non-homepage pages in Firefox
- Fixed "favicon not found" error on non-homepage pages, particularly in Firefox.
- Problem traced to incorrect favicon path introduced in commit 58177ea.
- Updated path from absolute to relative to resolve the issue.
- Ensures favicons load correctly across all pages.

Fixes #507

_layouts/base.html
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link id="defaultIcon1" rel="shortcut icon" href="/assets/favicon.png" />
+        <link id="defaultIcon1" rel="shortcut icon" href="{{ site.baseurl }}assets/favicon_06.png" />
         <title>{{ page.title }}</title>
         <link href="{{ site.baseurl }}/css/googleFonts.css" rel="stylesheet" type="text/css" >
         <meta name="robots" content="index,follow,NOODP" />

js/main.js
@@ -19,19 +19,11 @@ $(document).ready(function () {
 
     var defaultIcon = document.querySelector('#defaultIcon1');
     if (defaultIcon) {
-        // Use absolute path for favicon
-        var faviconPath = '/assets/favicon_' + logoID + '.png';
-        
-        // Test if favicon exists before setting
-        var img = new Image();
-        img.onload = function() {
-            defaultIcon.href = faviconPath;
-        };
-        img.onerror = function() {
-            // Fallback to default favicon
-            defaultIcon.href = '/assets/favicon.png';
-        };
-        img.src = faviconPath;
+        var logoID = colorIndex + 1;
+        if (logoID < 10) {
+            logoID = "0" + logoID;
+        }
+        defaultIcon.href = 'assets/favicon_' + logoID + '.png';
     }
 
     var h = document.querySelector('.logo1').innerHTML;

commit 245ea8cdae5f20236727dcb2adca67b004d38d85

Fix dynamic favicon path handling for subdirectory pages
- Fixed issue with favicon path on subdirectory pages by dynamically extracting and updating favicon filename.
- Problem traced to previous commit 15f9065 which introduced static favicon path.
- Ensured favicon loads correctly across all pages in firefox.

Fixes #507

_layouts/base.html
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <link id="defaultIcon1" rel="shortcut icon" href="{{ site.baseurl }}assets/favicon_06.png" />
+        <link id="defaultIcon1" rel="shortcut icon" href="{{ site.baseurl }}/assets/favicon_06.png" />
         <title>{{ page.title }}</title>
         <link href="{{ site.baseurl }}/css/googleFonts.css" rel="stylesheet" type="text/css" >
         <meta name="robots" content="index,follow,NOODP" />

js/main.js
@@ -23,7 +23,13 @@ $(document).ready(function () {
         if (logoID < 10) {
             logoID = "0" + logoID;
         }
-        defaultIcon.href = 'assets/favicon_' + logoID + '.png';
+
+        // Extract the base path from the current favicon href
+        var currentHref = defaultIcon.getAttribute('href');
+        var basePath = currentHref.substring(0, currentHref.lastIndexOf('/') + 1);
+        
+        // Update only the filename portion while keeping the original path
+        defaultIcon.href = basePath + 'favicon_' + logoID + '.png';
     }
 
     var h = document.querySelector('.logo1').innerHTML;